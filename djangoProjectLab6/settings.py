cd777a0da733b13d68b5c386bbf1e8c07782c580
Damian Stępień <151776252+damianSte@users.noreply.github.com>
2025-01-17 20:57:26 +0100

    corrections

--- djangoProjectLab6/settings.py
+++ djangoProjectLab6/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'djangoProject.urls'
+ROOT_URLCONF = 'djangoProjectLab6.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'djangoProject.wsgi.application'
+WSGI_APPLICATION = 'djangoProjectLab6.wsgi.application'
 
 
 # Database
